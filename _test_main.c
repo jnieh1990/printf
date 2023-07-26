2e7a807aef4a3330150e7d1d39896ecddd94f43f
jnieh1990 <jnieh1990@gmail.com>
2023-07-26 05:59:57 -0700

    a

--- _test_main.c
+++ _test_main.c
@@ -7,7 +7,8 @@
  *
  * Return: Always 0
  */
-int main(void)
+/*
+ * int main(void)
 {
 	int len;
 	int len2;
@@ -15,14 +16,14 @@ int main(void)
  * unsigned int ui;
  *void *addr;
  */
-
+/*
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 /*
  * ui = (unsigned int)INT_MAX + 1024;
  * addr = (void *)0x7ffe637541f0;
  */
-	_printf("Length:[%d, %i]\n", len, len);
+/*	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
